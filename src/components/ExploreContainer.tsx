9b943c994e90982ed3cf1b2b7fc89e89b7a87fc8
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2020-02-20 13:20:49 +0200

    Local devlopment

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import './ExploreContainer.css';
 
-interface ContainerProps {
-  name: string;
-}
+class ExploreContainer extends React.Component {
+  componentDidMount () {
+    // @ts-ignore
+    const PixellotWebSDK = window['pixellot-web-sdk'];
+    const {
+        TagDecorator,
+        ClipCreateDecorator,
+    } = PixellotWebSDK.Decorators;
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
-  return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    const playerContainer = document.getElementById('player');
+    const player = PixellotWebSDK.Player(document.getElementById('player'));
+
+    // const tagPlayer = TagDecorator(player);
+    // const clipPlayer = ClipCreateDecorator(tagPlayer, {
+      // onClipCreateRequest: () => {}
+    // });
+
+    player.setTitle('Awesome event');
+    player.setStreamNames(['a', 'b', 'c']);
+
+
+    player.setSource('https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4');
+    player.play();
+  }
+
+  render () {
+    return (
+      <div className="aspect-ratio-box">
+        <div className="aspect-ratio-box-inside">
+            <div id="player"></div>
+        </div>
     </div>
-  );
+    );
+  }
 };
 
 export default ExploreContainer;
