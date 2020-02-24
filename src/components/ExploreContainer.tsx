38a9d4a967bd20bb57dfec3b077367e4bf483e2b
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2020-02-24 13:53:07 +0200

    Commit

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -13,26 +13,28 @@ class ExploreContainer extends React.Component {
     const playerContainer = document.getElementById('player');
     const player = PixellotWebSDK.Player(document.getElementById('player'));
 
-    // const tagPlayer = TagDecorator(player);
-    // const clipPlayer = ClipCreateDecorator(tagPlayer, {
-      // onClipCreateRequest: () => {}
-    // });
+    // player.setTitle('Awesome event');
+    // player.setStreamNames(['a', 'b', 'c']);
+    // player.setActiveStreamName('a');
+    // player.setSwitchSelectCallback((active: String) => clipPlayer.setActiveStreamName(active));
 
-    player.setTitle('Awesome event');
-    player.setStreamNames(['a', 'b', 'c']);
+    const clipPlayer = ClipCreateDecorator(player, {
+      onClipCreateRequest: () => {}
+    });
 
-
-    player.setSource('https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4');
-    player.play();
+    clipPlayer.setSource('https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4');
+    clipPlayer.play();
   }
 
   render () {
     return (
-      <div className="aspect-ratio-box">
-        <div className="aspect-ratio-box-inside">
+      <div className="player-container">
+        <div className="aspect-ratio-box">
+          <div className="aspect-ratio-box-inside">
             <div id="player"></div>
+          </div>
         </div>
-    </div>
+      </div>
     );
   }
 };
